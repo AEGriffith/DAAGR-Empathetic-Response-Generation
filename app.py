52d4e8a2810d9cf3d1dead646fa1dbd66fca4d69
Amanda Griffith <amandaegriffith@gmail.com>
2023-04-06 11:20:21 -0400

    added placeholder for instructions and link to post-survey. Also made chat boxes a fixed height.

--- app.py
+++ app.py
@@ -1,46 +1,35 @@
 import gradio as gr
-import tensorflow as tf
 import time
-import warnings
-import os
 from transformers import (
-    AutoTokenizer,
-    AutoModelForSeq2SeqLM,
     pipeline,
+    GenerationConfig,
     AutoModelForCausalLM,
-    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    TFAutoModelForSeq2SeqLM,
+    TFAutoModelForSequenceClassification,
 )
 
-# Warning Suppression
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-# warnings.filterwarnings('ignore', category=DeprecationWarning)
-# warnings.filterwarnings('ignore', category=FutureWarning)
-# warnings.filterwarnings('ignore', category=UserWarning)
-# tf.get_logger().setLevel('ERROR')
-
 # emotion classification
-emotion_tokenizer = "aegrif/CIS6930_DAAGR_Classification"
-emotion_model = "aegrif/CIS6930_DAAGR_Classification"
+emotion_model = TFAutoModelForSequenceClassification.from_pretrained("aegrif/CIS6930_DAAGR_Classification")
+emotion_tokenizer = AutoTokenizer.from_pretrained("aegrif/CIS6930_DAAGR_Classification")
 emotion_pipeline = pipeline("text-classification", model=emotion_model, tokenizer=emotion_tokenizer)
 
 # generation models
 
-# no emotion
-gpt2_model_no_emo = "aegrif/CIS6930_DAAGR_GPT2_NoEmo"
-gpt2_tokenizer_no_emo = "aegrif/CIS6930_DAAGR_GPT2_NoEmo"
-chatbot_gpt_no_emo = pipeline(model=gpt2_model_no_emo, tokenizer=gpt2_tokenizer_no_emo, pad_token_id=50256)
-
-
 # decoder
-gpt2_model_emo = "aegrif/CIS6930_DAAGR_GPT2_Emo"
-gpt2_tokenizer = "aegrif/CIS6930_DAAGR_GPT2_Emo"
-chatbot_gpt_emo = pipeline(model=gpt2_model_emo, tokenizer=gpt2_tokenizer, pad_token_id=50256)
-
+gpt2_model_emo = AutoModelForCausalLM.from_pretrained("aegrif/CIS6930_DAAGR_GPT2_Emo")
+gpt2_tokenizer = AutoTokenizer.from_pretrained("aegrif/CIS6930_DAAGR_GPT2_Emo")
+chatbot_gpt_emo = pipeline("text-generation", model=gpt2_model_emo, tokenizer=gpt2_tokenizer, pad_token_id=50256)
 
 # encoder-decoder
-t5_model_emo = "aegrif/CIS6930_DAAGR_T5_Emo"
-t5_tokenizer = "t5-small"
-chatbot_t5_emo = pipeline(model=t5_model_emo, tokenizer=t5_tokenizer)
+t5_model_emo = TFAutoModelForSeq2SeqLM.from_pretrained("aegrif/CIS6930_DAAGR_T5_Emo")
+t5_tokenizer = AutoTokenizer.from_pretrained("t5-small")
+chatbot_t5_emo = pipeline("text2text-generation", model=t5_model_emo, tokenizer=t5_tokenizer)
+
+# no emotion
+t5_model_no_emo = TFAutoModelForSeq2SeqLM.from_pretrained("aegrif/CIS6930_DAAGR_T5_NoEmo")
+t5_model_no_emo.generation_config = GenerationConfig.from_pretrained("aegrif/CIS6930_DAAGR_T5_NoEmo")
+chatbot_t5_no_emo = pipeline("text2text-generation", model=t5_model_no_emo, tokenizer=t5_tokenizer)
 
 emotion_dict = {'disappointed': 0, 'annoyed': 1, 'excited': 2, 'afraid': 3, 'disgusted': 4, 'grateful': 5,
                 'impressed': 6, 'prepared': 7}
@@ -48,7 +37,6 @@ inverted_emotion_dict = {v: k for k, v in emotion_dict.items()}
 
 
 def get_context(user_input):
-
     output = emotion_pipeline(user_input)[0]['label']
 
     context = inverted_emotion_dict.get(int(output[-1]))
@@ -56,23 +44,23 @@ def get_context(user_input):
     return context
 
 
-def predict_gpt2_no_emo(user_input, history):
-    user_input = "<|user|>" + user_input + " <|bot|>"
+def predict_t5_no_emo(user_input, history):
+    # Get the context from the user input
+    context = get_context(user_input)
 
-    output = chatbot_gpt_no_emo(
+    user_input = f"question: {user_input} context: {context} </s>"
+    # Generate a response using the T5 model
+    bot_response = chatbot_t5_no_emo(
         user_input,
         max_new_tokens=40,
         num_return_sequences=1,
         do_sample=True,
-        temperature=0.5,
+        temperature=0.9,
         renormalize_logits=True,
-        exponential_decay_length_penalty=(5, 1.1),
+        exponential_decay_length_penalty=(20, 1),
         no_repeat_ngram_size=3,
         repetition_penalty=1.5
-    )
-
-    # Decode the generated response
-    bot_response = output[0]['generated_text'].split("<|bot|>")[1].strip()
+    )[0]['generated_text']
 
     return bot_response
 
@@ -89,9 +77,9 @@ def predict_gpt2(user_input, history):
         max_new_tokens=40,
         num_return_sequences=1,
         do_sample=True,
-        temperature=0.5,
+        temperature=0.9,
         renormalize_logits=True,
-        exponential_decay_length_penalty=(5, 1.1),
+        exponential_decay_length_penalty=(20, 1.05),
         no_repeat_ngram_size=3,
         repetition_penalty=1.5
     )
@@ -110,12 +98,13 @@ def predict_t5(user_input, history):
     # Generate a response using the T5 model
     bot_response = chatbot_t5_emo(
         user_input,
-        max_new_tokens=40,
+        max_new_tokens=60,
+        max_length=160,
         num_return_sequences=1,
         do_sample=True,
-        temperature=0.5,
+        temperature=0.9,
         renormalize_logits=True,
-        exponential_decay_length_penalty=(5, 1),
+        exponential_decay_length_penalty=(20, 1),
         no_repeat_ngram_size=3,
         repetition_penalty=1.5
     )[0]['generated_text']
@@ -127,12 +116,13 @@ def user(user_message, history):
     return "", history + [[user_message, None]]
 
 
-def gpt2_bot_no_emo(history):
+def t5_bot_no_emo(history):
     user_message = history[-1][0]
-    bot_message = predict_gpt2_no_emo(user_message, history)
+    bot_message = predict_t5_no_emo(user_message, history)
     history[-1][1] = bot_message
     time.sleep(1)
     return history
+
 
 def gpt2_bot(history):
     user_message = history[-1][0]
@@ -152,18 +142,23 @@ def t5_bot(history):
 
 with gr.Blocks() as demo:
     with gr.Row():
+        gr.Image("instructions.png", interactive=False, show_label=False)
+    with gr.Row():
         with gr.Column():
-            chatbot1 = gr.Chatbot().style()
+            chatbot1 = gr.Chatbot(label="Chatbot #1").style(height=500)
             msg1 = gr.Textbox(show_label=False, placeholder="Enter text and press enter").style(container=False)
         with gr.Column():
-            chatbot2 = gr.Chatbot().style()
+            chatbot2 = gr.Chatbot(label="Chatbot #2").style(height=500)
             msg2 = gr.Textbox(show_label=False, placeholder="Enter text and press enter").style(container=False)
         with gr.Column():
-            chatbot3 = gr.Chatbot().style()
+            chatbot3 = gr.Chatbot(label="Chatbot #3").style(height=500)
             msg3 = gr.Textbox(show_label=False, placeholder="Enter text and press enter").style(container=False)
+    with gr.Row():
+        gr.HTML('<p style="font-size:150%; font-family: "Playfair Display", "Didot", "Times New Roman">Once you have finished interacting with the agents, please follow the link below to complete a short survey about your experience.</p>'
+                '<p style="font-size:125%; font-family: "Playfair Display", "Didot", "Times New Roman"><a href=''https://docs.google.com/forms/d/1SICfdLcj_jbDeObZ6lxZ7b8a1L7fsZjX_ETfWc5o4VQ/edit'' target=''_blank''>https://docs.google.com/forms/d/1SICfdLcj_jbDeObZ6lxZ7b8a1L7fsZjX_ETfWc5o4VQ/edit</a></p')
 
     msg1.submit(user, [msg1, chatbot1], [msg1, chatbot1], queue=False).then(
-        gpt2_bot_no_emo, chatbot1, chatbot1
+        t5_bot_no_emo, chatbot1, chatbot1
     )
     msg2.submit(user, [msg2, chatbot2], [msg2, chatbot2], queue=False).then(
         gpt2_bot, chatbot2, chatbot2
